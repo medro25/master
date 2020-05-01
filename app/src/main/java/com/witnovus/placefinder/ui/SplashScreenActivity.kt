9e9f1de8291fc557c18cda093818d572364c0979
bhavikgoswami <bhavik.9oswami@gmail.com>
2020-05-01 15:25:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/witnovus/placefinder/ui/SplashScreenActivity.kt
@@ -0,0 +1,61 @@
+package com.witnovus.placefinder.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.witnovus.placefinder.MainActivity
+import com.witnovus.placefinder.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /* window.requestFeature(Window.FEATURE_NO_TITLE)
+         //making this activity full screen
+         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)*/
+         setContentView(R.layout.activity_splash_screen)
+        //Animation
+        addAnimationOperations()
+    }
+
+
+      private fun addAnimationOperations() {
+          Handler().postDelayed({ gotoNextActivity() }, 3000)
+          val constraint1 = ConstraintSet()
+          constraint1.clone(findViewById<ConstraintLayout>(R.id.root))
+
+          val constraint2 = ConstraintSet()
+          constraint2.clone(this, R.layout.splash_screen_alt)
+
+          Handler().postDelayed({
+              val autoTransition = AutoTransition()
+              autoTransition.duration = 2000
+              TransitionManager.beginDelayedTransition(
+                  findViewById<ConstraintLayout>(R.id.root),
+                  autoTransition
+              )
+              constraint2.applyTo(findViewById(R.id.root))
+          }, 500)
+      }
+
+    private fun gotoNextActivity() {
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            //finish this activity
+            finish()
+        }, 5000)
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+    }
+}
---
+++ app/src/main/java/com/witnovus/placefinder/ui/user/RegistrationActivity.kt
@@ -0,0 +1,13 @@
+package com.witnovus.placefinder.ui.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.witnovus.placefinder.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
